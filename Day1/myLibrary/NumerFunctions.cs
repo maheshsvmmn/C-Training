61dbcd06971a25c1e71b7b36900e38cb65b023cc
Mahesh Sharma <Mahesh.Sharma@avl.com>
2023-10-31 22:05:46 +0530

    Assignment 5 added

---
+++ Day1/myLibrary/NumerFunctions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myLibrary
+{
+    public static class NumerFunctions
+    {
+        public static int Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        public static float Division(float x, float y)
+        {
+            return x / y;
+        }
+        public static int Max(int x, int y)
+        {
+            if (x > y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+        }
+        public static int Min(int x, int y)
+        {
+            if (x > y)
+            {
+                return y;
+            }
+            else
+            {
+                return x;
+            }
+        }
+        public static bool Even(int x)
+        {
+            if (x % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool Odd(int x)
+        {
+            if (x % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool Prime(int x)
+        {
+            int cnt = 0;
+            for (int i = 2; i < x / 2; i++)
+            {
+                if (x % i == 0)
+                {
+                    cnt++;
+                }
+            }
+            if (cnt > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Day1/myLibrary/StringFunctions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myLibrary
+{
+    public static class StringFunctions
+    {
+        public static int GetCharacterCount(string str)
+        {
+            return str.Length;
+        }
+
+        public static bool IsPalindrome(string str)
+        {
+            int i = 0;
+            int j = str.Length - 1;
+            
+            while(i < j)
+            {
+                if (str[i] != str[j]) return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+        public static string Reverse(string str)
+        {
+            Console.WriteLine(str);
+            StringBuilder sb = new StringBuilder(""); 
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                sb.Append(str[i]);
+            }
+            return (sb.ToString());
+        }
+
+        public static string GetVowelsConsonantsNumbersSpecialChars(string str)
+        {
+            int vowels = 0, consonants = 0, numbers = 0, specials = 0;
+            string vowelString = "aeiou";
+            string consonantString = "bcdfghjklmnpqrstvwxyz";
+
+            foreach(var ch in str)
+            {
+                if(vowelString.Contains(ch)){
+                    vowels++;
+                }
+                else if(consonantString.Contains(ch)){
+                    consonants++;
+                }
+                else if(ch >= '0' && ch <= '9')
+                {
+                    numbers++;
+                }
+                else
+                {
+                    specials++;
+                }
+            }
+
+            return $"\nVowels : {vowels}\nConsonants : {consonants}\nNumbers : {numbers}\nSpecial Characters : {specials}\n";
+        }
+
+
+        public static string ConvertToLowerCase(string str)
+        {
+            return str.ToLower();
+        }
+        
+        public static string ConvertToUpperCase(string str)
+        {
+            return str.ToUpper();
+        }
+
+        public static string Combine(string str1 , string str2)
+        {
+            return string.Concat(str1, str2);
+        }
+
+        
+    }
+}
